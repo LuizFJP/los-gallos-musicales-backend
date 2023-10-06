e266aa1ecf64d244d4a22d9fdc7c0c85e4ea0b83
gusdev0258 <16326176760@edu.udesc.br>
2023-10-05 21:18:26 -0300

    creatiing image upload

--- src/api/routes/ImageRoute.ts
+++ src/api/routes/ImageRoute.ts
@@ -1,28 +1,35 @@
+import { Request, RequestHandler, Response } from "express";
+import { multerConfig } from "../../config/MulterConfig";
 import { ImageController } from "../controller/ImageController";
 import { Routes } from "./Routes";
-import multer from 'multer';
-
+import multer from "multer";
 export class ImageRoute {
-    private routes: Routes;
-    private imageController: ImageController;
+  private routes: Routes;
+  private imageController: ImageController;
 
-    constructor(routes: Routes) {
-        this.routes = routes;
-        this.imageController = new ImageController();
-        this.init();
-    }
+  constructor(routes: Routes) {
+    this.routes = routes;
+    this.imageController = new ImageController();
+    this.init();
+  }
 
-    init() {
-        const route = this.routes.getRouter();
-        route.use('/image', route);
+  init() {
+    const route = this.routes.getRouter();
+    route.use("/image", route);
 
-        route.post('/upload',multer(multer) ,async (req, res) => {
-            console.log(req.body);
+    const uploadMiddleware = multer(multerConfig).single("image");
 
-            await this.imageController.uploadImage();
-        })
+    const uploadHandler: RequestHandler = async (req: Request, res: Response) => {
+    console.log(req.file);
+    //   await this.imageController.uploadImage();
+    };
 
-   getRouter(): Routes {
-        return this.routes;
-    }
+    route.post("/upload", uploadMiddleware, uploadHandler);
+
+    return route;
+  }
+
+  getRouter(): Routes {
+    return this.routes;
+  }
 }
