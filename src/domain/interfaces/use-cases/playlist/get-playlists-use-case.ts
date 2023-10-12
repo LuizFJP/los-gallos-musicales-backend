af1a6bd61134bfe301b6ce8f46ab4689588ad3cc
gusdev0258 <16326176760@edu.udesc.br>
2023-10-11 23:43:20 -0300

    feat: get playlists by id

--- src/domain/interfaces/use-cases/playlist/get-playlists-use-case.ts
+++ src/domain/interfaces/use-cases/playlist/get-playlists-use-case.ts
@@ -1,4 +1,4 @@
-import { SongDTO } from ".../../../dto/playlist/song";
+import { SongDTO } from "../../../dto/playlist/song";
 
 export interface GetPlaylistsUseCase {
   execute(name: string): Promise<SongDTO[]>;
