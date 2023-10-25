6eab3051612d3bc2ff08527c9a8bda1cf8b44866
gusdev0258 <16326176760@edu.udesc.br>
2023-10-25 00:33:46 -0300

    creating usecase of not same player in one room

--- src/domain/use-cases/room/enter-room.ts
+++ src/domain/use-cases/room/enter-room.ts
@@ -13,6 +13,9 @@ export class EnterRoom implements EnterRoomUserCase {
     try {
       const room = await this.roomRepository.get(name);
       const roomParsed = JSON.parse(room);
+      if (roomParsed.players.includes(player)) {
+        pla
+      }
       roomParsed.players.push(player);
       const roomStringify = JSON.stringify(roomParsed);
       await this.roomRepository.create(roomParsed.name, roomStringify);
