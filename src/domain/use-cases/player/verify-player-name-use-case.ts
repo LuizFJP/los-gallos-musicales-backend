b6451dbe2c31768f92fb31b8dbf6bf3bb1ed3c03
gusdev0258 <16326176760@edu.udesc.br>
2023-10-29 16:14:19 -0300

    feat: verify duplicate name and create new name

--- src/domain/use-cases/player/verify-player-name-use-case.ts
+++ src/domain/use-cases/player/verify-player-name-use-case.ts
@@ -11,12 +11,20 @@ export class VerifyPlayerName implements VerifyPlayerNameUseCase {
 
   async execute(username: string, roomName: string): Promise<any> {
     try {
+      console.log("aqui: " + username);
+      console.log("la: " + roomName);
       const room = await this.roomRepository.get(roomName);
-      const roomParsed = JSON.parse(room);
-      const playerExists = roomParsed.players.find((p: Player) => p.username === username);
-      let newUsername: string = '';
-      playerExists ? newUsername = `${username}#${Math.floor(Math.random() * 1000)}` : newUsername = username;
-      return newUsername;
+      if(room) {
+        const roomParsed = JSON.parse(room);
+        console.log("roomParsed: " + roomParsed)
+        const playerExists = roomParsed.players.find((p: Player) => p.username === username);
+        console.log("playerExists: " + playerExists)
+        let newUsername: string = '';
+        playerExists ? newUsername = `${username}#${Math.floor(Math.random() * 1000)}` : newUsername = username;
+        console.log("newUsername: " + newUsername)
+        return newUsername;
+      }
+      return username;
     } catch (error) {
       return null;
     }
